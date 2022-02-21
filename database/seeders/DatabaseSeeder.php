3b5098508ea37aecf38dd597e83cbcb4739efdbc
BosToken <faizdiandra@gmail.com>
2022-02-21 17:06:36 +0700

    pgsql

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(BankAccountSeeder::class);
         // $this->call(ProductSeeder::class);
         $this->call(GenderSeeder::class);
-        // $this->call(TypeSeeder::class);
+        $this->call(TypeSeeder::class);
         // $this->call(CartSeeder::class);
         $this->call(SettingSeeder::class);
     }
